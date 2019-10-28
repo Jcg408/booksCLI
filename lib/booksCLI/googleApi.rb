1e09a007757879b9aec6b0ecaf74186935a504fc
Jessica Gladstone <jessgladtobe@hotmail.com>
2019-10-27 21:13:07 -0400

    clean up methods for data fetch and iteration

--- lib/booksCLI/googleApi.rb
+++ lib/booksCLI/googleApi.rb
@@ -13,18 +13,17 @@ class BooksCLI::GoogleApi
    def fetch_data
     url = "https://www.googleapis.com/books/v1/volumes?q=#{@input}&fields=items(volumeInfo/title, volumeInfo/authors, volumeInfo/publisher)&maxResults=5&key=AIzaSyA-h3olrB1IYcizfPJcMADnlT161h98fzo"
     response = HTTParty.get(url)
-        response["items"].each_with_index do |item, index|
-            title =  item["volumeInfo"]["title"]
-            authors = item["volumeInfo"]["authors"]
-            publisher = item["volumeInfo"]["publisher"]
-            binding.pry
+   end
+
+   def set_info
+        self.fetch_data["items"].each_with_index do |item, index|
+            book = BooksCLI::Books.new
+            book.title =  item["volumeInfo"]["title"]
+            book.authors = item["volumeInfo"]["authors"]
+            book.publisher = item["volumeInfo"]["publisher"]
+          
         end
+     
     end
-
-    def set_info
-        puts self.fetch_data.title
-
-    end
-
    
 end
